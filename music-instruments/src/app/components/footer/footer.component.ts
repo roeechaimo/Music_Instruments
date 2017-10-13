4a20c7e47d41d32c9917e20f59bb1f5a176a0f2a
roeechaimo <roee84@gmail.com>
2017-10-13 14:48:20 +0300

    change footer content

--- music-instruments/src/app/components/footer/footer.component.ts
+++ music-instruments/src/app/components/footer/footer.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: 'footer.component.html'  
+  templateUrl: 'footer.component.html'
 })
 export class FooterComponent {
-  footer = 'This is Footer!';
+  footer = 'Play your favourite instrument!';
 }
